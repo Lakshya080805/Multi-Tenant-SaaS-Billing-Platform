b81a68601b854a182e2af7defa9e4baccde199b2
Lakshya080805 <lakshyasinghal08@gmail.com>
2026-03-10 23:00:42 +0530

    disable file logging in production for render deployment

--- src/config/logger.js
+++ src/config/logger.js
@@ -25,19 +25,20 @@ export const logger = winston.createLogger({
     json()
   ),
   transports: [
-    new winston.transports.Console({ format: consoleFormat }),
-    ...(process.env.NODE_ENV !== 'test'
-      ? [
-          new winston.transports.File({
-            filename: 'logs/error.log',
-            level: 'error'
-          }),
-          new winston.transports.File({
-            filename: 'logs/combined.log'
-          })
-        ]
-      : [])
-  ]
+  new winston.transports.Console({ format: consoleFormat }),
+
+  ...(process.env.NODE_ENV === 'development'
+    ? [
+        new winston.transports.File({
+          filename: 'logs/error.log',
+          level: 'error'
+        }),
+        new winston.transports.File({
+          filename: 'logs/combined.log'
+        })
+      ]
+    : [])
+]
 });
 
 export const httpLogger = {
